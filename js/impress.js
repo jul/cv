c32e14196eb93794ed047329e828fd15c88e7989
julien tayon <jtayon@gmail.com>
2012-11-22 23:07:08 +0100

    thx tidy ... a bug in the html is a bug everywhere

--- js/impress.js
+++ js/impress.js
@@ -684,7 +684,7 @@
         
         // Prevent default keydown action when one of supported key is pressed.
         document.addEventListener("keydown", function ( event ) {
-            if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
+            if ( event.keyCode === 9 || event.keyCode === 32 || event.keyCode === 37 || event.keyCode === 39) { 
                 event.preventDefault();
             }
         }, false);
@@ -705,23 +705,18 @@
         //   as another way to moving to next step... And yes, I know that for the sake of
         //   consistency I should add [shift+tab] as opposite action...
         document.addEventListener("keyup", function ( event ) {
-            if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
+            if ( event.keyCode === 37 || event.keyCode === 32 || event.keyCode ==39) { 
                 switch( event.keyCode ) {
-                    case 33: // pg up
                     case 37: // left
-                    case 38: // up
                              api.prev();
                              break;
-                    case 9:  // tab
                     case 32: // space
-                    case 34: // pg down
                     case 39: // right
-                    case 40: // down
                              api.next();
                              break;
                 }
                 
-                event.preventDefault();
+              /*  event.preventDefault();*/
             }
         }, false);
         
