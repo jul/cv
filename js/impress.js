802f1e31b62ac72e16267dad16cfc962d8964b01
julien tayon <julien@tayon.net>
2016-08-04 14:09:49 +0200

    adding new experience Correcting a bad merge. I hate git simple complex ways

--- js/impress.js
+++ js/impress.js
@@ -684,11 +684,7 @@
         
         // Prevent default keydown action when one of supported key is pressed.
         document.addEventListener("keydown", function ( event ) {
-<<<<<<< HEAD
-            if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
-=======
             if ( event.keyCode === 9 || event.keyCode === 32 || event.keyCode === 37 || event.keyCode === 39) { 
->>>>>>> gh-pages
                 event.preventDefault();
             }
         }, false);
@@ -709,20 +705,6 @@
         //   as another way to moving to next step... And yes, I know that for the sake of
         //   consistency I should add [shift+tab] as opposite action...
         document.addEventListener("keyup", function ( event ) {
-<<<<<<< HEAD
-            if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
-                switch( event.keyCode ) {
-                    case 33: // pg up
-                    case 37: // left
-                    case 38: // up
-                             api.prev();
-                             break;
-                    case 9:  // tab
-                    case 32: // space
-                    case 34: // pg down
-                    case 39: // right
-                    case 40: // down
-=======
             if ( event.keyCode === 37 || event.keyCode === 32 || event.keyCode ==39) { 
                 switch( event.keyCode ) {
                     case 37: // left
@@ -730,16 +712,11 @@
                              break;
                     case 32: // space
                     case 39: // right
->>>>>>> gh-pages
                              api.next();
                              break;
                 }
                 
-<<<<<<< HEAD
-                event.preventDefault();
-=======
               /*  event.preventDefault();*/
->>>>>>> gh-pages
             }
         }, false);
         
